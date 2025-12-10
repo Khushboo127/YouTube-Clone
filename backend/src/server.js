ae3bae67f334d85133c3aabaa4139001c06209c8
Khsuhboo127 <vermakhushboo845@gmail.com>
2025-12-10 20:24:34 +0530

    basic express server setup

---
+++ backend/src/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import channelRoutes from "./routes/channelRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// middleware
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/channels", channelRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+app.use(
+    "/uploads",
+    express.static(path.join(__dirname, "..", "uploads"))
+);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log(`Server running on port ${PORT}`);
+});
